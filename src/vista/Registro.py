4437891a4c241acf7e25d6d1c7f3607e3f01c53b
Mario Fernández Costello <mfcostello@MacBook-Air-de-Mario-5.local>
2025-05-13 17:45:26 +0200

    cambio

--- src/vista/Registro.py
+++ src/vista/Registro.py
@@ -1,9 +1,1 @@
 class Registro():
-    def __init__(self, nombre, apellido, edad, dni):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.dni = dni
-
-    def __str__(self):
-        return f"Nombre: {self.nombre}, Apellido: {self.apellido}, Edad: {self.edad}, DNI: {self.dni}"
