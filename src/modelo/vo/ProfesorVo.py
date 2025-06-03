4f40d7a8cf598553e153cc2c0554583b6222248c
Jaime Carnicero <jcarnf00@estudiantes.unileon.es>
2025-06-04 01:04:32 +0200

    TUI aleatorio

--- src/modelo/vo/ProfesorVo.py
+++ src/modelo/vo/ProfesorVo.py
@@ -1,5 +1,6 @@
 class ProfesorVo:
-    def __init__(self, id_usuario, grado_academico, saldo=0.0):
+    def __init__(self, id_usuario, grado_academico, tui_numero, saldo=0.0):
         self.id_usuario = id_usuario
         self.grado_academico = grado_academico
+        self.tui_numero = tui_numero
         self.saldo = saldo
